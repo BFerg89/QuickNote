9de95bcc5ff815f236aa11187cfde89b3d5ab45d
Bennett Ferguson <bennett2007@outlook.com>
2026-08-09 09:47:35 -0700

    gitignore

---
+++ QuickNote/Views/Notes/NotesView.swift
@@ -0,0 +1,18 @@
+//
+//  NotesView.swift
+//  QuickNote
+//
+//  Created by Bennett Ferguson on 2026-08-09.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotesView()
+}
